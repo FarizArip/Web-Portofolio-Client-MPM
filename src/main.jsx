5730f061db7c4aee0372ce357dd0bdb7cc27243b
FarizArip <101920335+FarizArip@users.noreply.github.com>
2026-06-11 22:16:35 +0700

    Pls fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
